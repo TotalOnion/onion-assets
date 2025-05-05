a2d6c2bc880035673e2bead35302755a694a781d
Ciaran Kamp <ciarankamp@gmail.com>
2025-05-05 12:10:06 +0100

    tidy log

--- js/standard-content-v2/standard-content-v2.js
+++ js/standard-content-v2/standard-content-v2.js
@@ -3,7 +3,6 @@
 export default function standardcontentV2Js(options = {}) {
 	try {
 		const {block} = options;
-		console.log("🚀 ~ standardcontentV2Js ~ block:", block)
 		const dataset = block.dataset;
 		if (dataset.enablevideoncontroller) {
 			import('@pernod-ricard-global-cms/cblvideocontroller')
